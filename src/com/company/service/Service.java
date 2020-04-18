52b66ae5933435590090de15aa2b08360decaa52
sniegapele <kotryna.slepetyte@gmail.com>
2020-04-18 16:50:15 +0300

    error handling added (invalid input)

--- src/com/company/service/Service.java
+++ src/com/company/service/Service.java
@@ -12,17 +12,21 @@ public class Service {
         this.scanner = new Scanner(System.in);
     }
 
-    public void run() throws IOException {
+    public void run() {
         while (true) {
             String urlAddress = scanner.nextLine();
             if (urlAddress.equals("q")) {
                 break;
             }
 
-            Protocol HTTPprotocol = new Protocol(urlAddress);
-            HTTPprotocol.transfer();
+            try {
+                Protocol HTTPprotocol = new Protocol(urlAddress);
+                HTTPprotocol.transfer();
 
-            System.out.println("Received");
+                System.out.println("Received");
+            } catch (Exception e) {
+                System.out.println("Invalid input");
+            }
         }
     }
 }
